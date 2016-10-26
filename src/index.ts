c6208be0465d6cbdef5562f23f181ac903d0e522
microshine <microshine@mail.ru>
2016-10-26 23:39:13 +0300

    Fix error for travis

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export * from "./base";
 export * from "./alg";
 export * from "./base64url";
 export * from "./subtle";
+export * from "./aes/crypto";
+export * from "./rsa/crypto";
+export * from "./ec/crypto";
+export * from "./sha/crypto";
