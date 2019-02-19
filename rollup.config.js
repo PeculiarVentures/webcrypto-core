7cbc4c7d93d346fe4d50ff907922125cdfa2cc0b
microshine <microshine@mail.ru>
2019-02-19 12:35:20 +0300

    Add ES2015 module

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,46 @@
 import typescript from "rollup-plugin-typescript";
+import ts from "typescript";
 
 const pkg = require("./package.json");
 
-const banner = [pkg.banner || "", ""];
+const banner = [
+  "/**",
+  " * Copyright (c) 2019, Peculiar Ventures, All rights reserved.",
+  " */",
+  "",
+];
+const input = "src/index.ts";
+const external = Object.keys(pkg.dependencies);
 
-export default {
-    input: "src/index.ts",
+export default [
+  // main
+  {
+    input,
     plugins: [
-        typescript({ typescript: require("typescript"), target: "esnext", removeComments: true }),
+      typescript({ typescript: ts, target: "esnext", removeComments: true }),
     ],
-    external: ["tslib", "pvtsutils"],
+    external,
     output: [
-        {
-            banner: banner.join("\n"),
-            file: pkg.main,
-            format: "cjs",
-        }
+      {
+        banner: banner.join("\n"),
+        file: pkg.main,
+        format: "cjs",
+      }
     ]
-};
+  },
+  // lib
+  {
+    input,
+    plugins: [
+      typescript({ typescript: ts, target: "esnext", removeComments: true }),
+    ],
+    external,
+    output: [
+      {
+        banner: banner.join("\n"),
+        file: pkg.module,
+        format: "es",
+      }
+    ]
+  },
+];
