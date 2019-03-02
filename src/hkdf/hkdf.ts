484f946a4147b5a91c78a27b88141056c25454fa
microshine <microshine@mail.ru>
2019-03-02 10:48:02 +0300

    Add HKDF provider

---
+++ src/hkdf/hkdf.ts
@@ -0,0 +1,40 @@
+import { KeyUsages } from "../../build/webcrypto-core";
+import { ProviderCrypto } from "../provider";
+import { BufferSourceConverter } from "../utils";
+
+export abstract class HkdfProvider extends ProviderCrypto {
+
+  public name = "HMAC";
+  public hashAlgorithms = ["SHA-1", "SHA-256", "SHA-384", "SHA-512"];
+  public usages: KeyUsages = ["deriveKey", "deriveBits"];
+
+  public checkAlgorithmParams(algorithm: HkdfParams) {
+    // hash
+    this.checkRequiredProperty(algorithm, "hash");
+    this.checkHashAlgorithm(algorithm.hash as Algorithm, this.hashAlgorithms);
+
+    // salt
+    this.checkRequiredProperty(algorithm, "salt");
+    if (BufferSourceConverter.isBufferSource(algorithm.salt)) {
+      throw new TypeError("salt: Is not of type '(ArrayBuffer or ArrayBufferView)'");
+    }
+
+    // info
+    this.checkRequiredProperty(algorithm, "info");
+    if (BufferSourceConverter.isBufferSource(algorithm.info)) {
+      throw new TypeError("salt: Is not of type '(ArrayBuffer or ArrayBufferView)'");
+    }
+  }
+
+  public checkImportKey(format: KeyFormat, keyData: JsonWebKey | ArrayBuffer, algorithm: Algorithm, extractable: boolean, keyUsages: KeyUsage[]) {
+    super.checkImportKey(format, keyData, algorithm, extractable, keyUsages);
+    if (extractable) {
+      // If extractable is not false, then throw a SyntaxError
+      throw new SyntaxError("extractable: Must be False");
+    }
+  }
+
+  public abstract onImportKey(format: KeyFormat, keyData: JsonWebKey | ArrayBuffer, algorithm: Algorithm, extractable: boolean, keyUsages: KeyUsage[]): Promise<CryptoKey>;
+  public abstract onDeriveBits(algorithm: Pbkdf2Params, baseKey: CryptoKey, length: number): Promise<ArrayBuffer>;
+
+}
