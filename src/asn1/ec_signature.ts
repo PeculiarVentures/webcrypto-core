1d5dfcc77c0e81ca75bed0b9f00f63ac560158e0
microshine <microshine@mail.ru>
2020-04-05 15:00:33 +0300

    Add ASN1 schemas for RSA and EC objects

---
+++ src/asn1/ec_signature.ts
@@ -0,0 +1,75 @@
+import { AsnProp, AsnPropTypes } from "@peculiar/asn1-schema";
+import { BufferSourceConverter } from "pvtsutils";
+import { AsnIntegerWithoutPaddingConverter } from "./converters";
+
+// RFC 3279
+// https://tools.ietf.org/html/rfc3279#section-2.2.3
+//
+// ECDSA-Sig-Value ::= SEQUENCE {
+//   r  INTEGER,
+//   s  INTEGER
+// }
+
+
+
+export class EcDsaSignature {
+
+  public static fromWebCryptoSignature(value: BufferSource): EcDsaSignature {
+    const wcSignature = BufferSourceConverter.toUint8Array(value);
+    const pointSize = wcSignature.byteLength / 2;
+    const ecSignature = new this();
+    ecSignature.r = ecSignature.removePadding(wcSignature.slice(0, pointSize));
+    ecSignature.s = ecSignature.removePadding(wcSignature.slice(pointSize, pointSize * 2));
+    return ecSignature;
+  }
+
+  @AsnProp({ type: AsnPropTypes.Integer, converter: AsnIntegerWithoutPaddingConverter })
+  public r = new ArrayBuffer(0);
+
+  @AsnProp({ type: AsnPropTypes.Integer, converter: AsnIntegerWithoutPaddingConverter })
+  public s = new ArrayBuffer(0);
+
+  public toWebCryptoSignature(pointSize?: number) {
+    pointSize = this.getPointSize();
+    const r = this.addPadding(pointSize, BufferSourceConverter.toUint8Array(this.r));
+    const s = this.addPadding(pointSize, BufferSourceConverter.toUint8Array(this.s));
+
+    const wcSignature = new Uint8Array(r.byteLength + s.byteLength);
+    wcSignature.set(r, 0);
+    wcSignature.set(s, r.length);
+    return wcSignature.buffer;
+  }
+
+  private getPointSize(): number {
+    // tslint:disable-next-line: no-bitwise
+    const size = Math.max(this.r.byteLength, this.s.byteLength) << 3;
+    switch (size) {
+      case 32:
+        return 32;
+      case 48:
+        return 48;
+      case 66:
+        return 66;
+    }
+    throw new Error("Method not implemented.");
+  }
+
+  private addPadding(pointSize: number, bytes: BufferSource) {
+    const res = new Uint8Array(pointSize);
+    const uint8Array = BufferSourceConverter.toUint8Array(bytes);
+    res.set(uint8Array, pointSize - uint8Array.length);
+    return res;
+  }
+
+  private removePadding(bytes: BufferSource) {
+    const uint8Array = BufferSourceConverter.toUint8Array(bytes);
+    for (let i = 0; i < uint8Array.length; i++) {
+      if (!uint8Array[i]) {
+        continue;
+      }
+      return uint8Array.slice(i);
+    }
+    return new Uint8Array(0);
+  }
+
+}
